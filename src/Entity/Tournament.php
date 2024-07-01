ec0464634fd8ebcb6331b562083b6c18ec375d73
Kodpi <@isamposbe0>
2024-07-01 17:41:58 +0300

    Добавим таблицу турниров

---
+++ src/Entity/Tournament.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TournamentRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TournamentRepository::class)]
+class Tournament
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    /**
+     * @var Collection<int, Tour>
+     */
+    #[ORM\OneToMany(targetEntity: Tour::class, mappedBy: 'tornament')]
+    private Collection $tours;
+
+    public function __construct()
+    {
+        $this->tours = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Tour>
+     */
+    public function getTours(): Collection
+    {
+        return $this->tours;
+    }
+
+    public function addTour(Tour $tour): static
+    {
+        if (!$this->tours->contains($tour)) {
+            $this->tours->add($tour);
+            $tour->setTornament($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTour(Tour $tour): static
+    {
+        if ($this->tours->removeElement($tour)) {
+            // set the owning side to null (unless already changed)
+            if ($tour->getTornament() === $this) {
+                $tour->setTornament(null);
+            }
+        }
+
+        return $this;
+    }
+}
