e8f4a35f1a6c673012dbd38116122b69aa9284cc
Kodpi <@isamposbe0>
2024-07-01 17:42:24 +0300

    Добавим таблицу туров для турнира

---
+++ src/Entity/Tour.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TourRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TourRepository::class)]
+class Tour
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\ManyToOne(inversedBy: 'tours')]
+    private ?Tournament $tornament = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getTornament(): ?Tournament
+    {
+        return $this->tornament;
+    }
+
+    public function setTornament(?Tournament $tornament): static
+    {
+        $this->tornament = $tornament;
+
+        return $this;
+    }
+}
