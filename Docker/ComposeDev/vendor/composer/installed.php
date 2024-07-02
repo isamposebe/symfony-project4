607fd0a82802ce4f25dd1ac69d6e458aa0ce205a
Kodpi <@isamposbe0>
2024-07-02 10:45:45 +0300

    Добавляю настройки чтобы работало

---
+++ Docker/ComposeDev/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'afd7946c09d52d795b976d75f00c489228298a93',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'afd7946c09d52d795b976d75f00c489228298a93',
+            'dev_requirement' => false,
+        ),
+    ),
+);
