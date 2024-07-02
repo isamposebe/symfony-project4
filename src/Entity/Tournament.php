5bf00973ec4aa16069e061b47ebb254a85af2932
Kodpi <@isamposbe0>
2024-07-02 16:53:19 +0300

    Поправлена сущности

--- src/Entity/Tournament.php
+++ src/Entity/Tournament.php
@@ -21,13 +21,12 @@ class Tournament
     #[ORM\Column(type: 'string', length: 255, nullable: false)]
     private string $name;
 
-
-    public function getId(): ?int
+    public function getId(): int
     {
         return $this->id;
     }
 
-    public function getName(): ?string
+    public function getName(): string
     {
         return $this->name;
     }
