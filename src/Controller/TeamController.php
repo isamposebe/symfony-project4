44cd82808a835c9eed415ee319a1a6bf2677655d
Kodpi <@isamposbe0>
2024-07-02 12:16:15 +0300

    Создание новой команды

---
+++ src/Controller/TeamController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Team;
+use App\Form\NewsType;
+use App\Form\TeamType;
+use App\Repository\TeamRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/team')]
+class TeamController extends AbstractController
+{
+    #[Route('/', name: 'app_team_index', methods: ['GET'])]
+    public function index(TeamRepository $teamRepository): Response
+    {
+        return $this->render('team/index.html.twig', [
+            'teams' => $teamRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_team_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $team = new Team();
+        $form = $this->createForm(TeamType::class, $team);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($team);
+            $entityManager->flush();
+
+            $this->addFlash(
+                'notice',
+                'Your changes were saved!'
+            );
+            /** Пустая форма для повторного ввода*/
+            $form = $this->createForm(TeamType::class, $team);
+        }
+
+        return $this->render('team/new.html.twig', [
+            'team' => $team,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_team_delete', methods: ['POST'])]
+    public function delete(Request $request, Team $team, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$team->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($team);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_team_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
