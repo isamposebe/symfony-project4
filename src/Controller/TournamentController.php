e0ae1a26dd9242e0c9b584754e564d3687245dd5
Kodpi <@isamposbe0>
2024-07-02 12:17:29 +0300

    Добавление кнопки для переходов на Создание новой команды и создание турнира

--- src/Controller/TournamentController.php
+++ src/Controller/TournamentController.php
@@ -2,17 +2,36 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[Route('/tournament',)]
+#[IsGranted('ROLE_USER')]
 class TournamentController extends AbstractController
 {
-    #[Route('/tournament', name: 'app_tournament')]
+    #[Route('/', name: 'app_tournament')]
     public function index(): Response
     {
+
+
         return $this->render('tournament/index.html.twig', [
             'controller_name' => 'TournamentController',
         ]);
+    }
+
+    /**
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @return Response
+     */
+    #[Route('/new', name: 'app_tournament_new')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+
+        return $this->render('tournament/new.html.twig', []);
     }
 }
