b5935ebea4292ab6f748f1a0beaae374fc3acc3d
Kodpi <@isamposbe0>
2024-07-03 16:08:22 +0300

    Почистил от лишнего и неиспользованного

--- src/Entity/Tour.php
+++ src/Entity/Tour.php
@@ -3,7 +3,6 @@
 namespace App\Entity;
 
 use App\Repository\TourRepository;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TourRepository::class)]
