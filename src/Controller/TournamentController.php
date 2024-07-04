1e788b903641878fa425cc3d900fabcfb9b33161
Kodpi <@isamposbe0>
2024-07-04 11:50:01 +0300

    Написал комментарии для кода и удалил не использованные элементы

--- src/Controller/TournamentController.php
+++ src/Controller/TournamentController.php
@@ -18,6 +18,9 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[IsGranted('ROLE_USER')]
 class TournamentController extends AbstractController
 {
+    /** Главная страница турнира
+     * @return Response
+     */
     #[Route('/', name: 'app_tournament')]
     public function index(): Response
     {
@@ -27,57 +30,78 @@ class TournamentController extends AbstractController
     }
 
     /**
-     * @param Request $request
-     * @param TournamentService $service
-     * @param EntityManagerInterface $entityManager
+     * @param Request $request Для проверки формы
+     * @param TournamentService $service Сервис по работе с турнирам
      * @return Response
      */
     #[Route('/new', name: 'app_tournament_new')]
-    public function new(Request $request,TournamentService $service, EntityManagerInterface $entityManager): Response
+    public function new(Request $request,TournamentService $service): Response
     {
         /** Создаем турнир */
         $tournament = new Tournament();
+
         /** Создание формы турнира */
         $formTournament = $this->createForm(TournamentType::class, $tournament);
         $formTournament->handleRequest($request);
 
+        /** Обработка нажатие кнопки из формы */
         if ($formTournament->isSubmitted() && $formTournament->isValid()) {
+
             /** Проверяем имя на повторы */
             if ($service->identityVerificationName($tournament)){
+
                 /** Записываем в базу данных */
                 $service->addItem($tournament);
+
+                /** Выводим сообщение об удачном сохранении */
                 $this->addFlash(
                     'notice',
                     'Your changes were saved!'
                 );
+
+                /** Переходим на страницу добавлении команд */
                 return $this->redirectToRoute('app_tournament_show',[
                     'id' => $tournament->getId(),
                 ]);
             }else{
+                /** Выводим сообщение об ошибке */
                 $this->addFlash(
                     'notice',
                     'Турнир с таким именем уже существует'
                 );
             }
-
         }
 
-
+        /** Отправляю данные в шаблон
+         * @formTournament Данные формы для турнира
+         */
         return $this->render('tournament/new.html.twig', [
             'formTournament' => $formTournament
         ]);
     }
+
+    /** Просмотр турнира и добавление в турнир команды
+     * @param int $id ID турнира
+     * @param TournamentService $service Сервис по работе с турниром
+     * @return Response
+     */
     #[Route('/show/{id}', name: 'app_tournament_show')]
     public function show(int $id, TournamentService $service): Response
     {
         /** Найдем турнир по id */
         $tournament = $service->searchTournamentID($id);;
 
+        /** Список команды в турнире */
         $listTeam = $service->listTourNumTournament($tournament);
 
         /** Форма для регистрации команды на турнир */
         $formTeam = $this->createForm(RecordingCommandType::class);
 
+        /** Отправляем данные в шаблон
+         * @formTeam Форма для добавления команды в турнир
+         * @tournament Данные турнира
+         * @listTeam Список команд в турнире
+         */
         return $this->render('tournament/show.html.twig', [
             'formTeam' => $formTeam,
             'tournament' => $tournament,
@@ -85,8 +109,13 @@ class TournamentController extends AbstractController
         ]);
     }
 
+    /** Добавление команды в турнире
+     * @param Request $request Данные request
+     * @param TournamentService $service Сервис по работе с турниром
+     * @return Response
+     */
     #[Route('/addTeamTournament/', name: 'app_add_team_tournament')]
-    public function addTeam(Request $request,TournamentService $service,EntityManagerInterface $entityManager): Response
+    public function addTeam(Request $request,TournamentService $service): Response
     {
         /** Получаем из request имя команды */
         $team = new Team();
@@ -96,11 +125,13 @@ class TournamentController extends AbstractController
         if ($service->identityVerificationName($team->setName($nameTeam))){
             return new Response('not Team', Response::HTTP_OK);
         }
-        $team = $service->searchTeam($nameTeam);
 
+        /** Ищем команду по имени и возвращаем команду из базы данных */
+        $team = $service->searchTeam($nameTeam);
 
         /** Получаем турнир по ID */
         $tournamentID = $request->request->get('tournamentID');
+        /** Берем турнир из базы данных */
         $tournament = $service->searchTournamentID($tournamentID);
 
         /** Записываем в базу данных добавление команды в турнир */
