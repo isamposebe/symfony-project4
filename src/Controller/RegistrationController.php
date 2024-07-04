9e5d9466a7aab7f5d0cbc79cf993a443ddcb3607
Kodpi <@isamposbe0>
2024-07-04 11:46:21 +0300

    Отдал ответственность на сервис

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -33,11 +33,9 @@ class RegistrationController extends AbstractController
                 )
             );
 
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            // do anything else you need here, like send an email
-
+            /** Записываем в базу данных пользователя */
+            $service->addItem($user);
+            /** Переходим обратно в Login */
             return $security->login($user, 'form_login', 'main');
         }
 
