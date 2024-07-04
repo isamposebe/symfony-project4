62b3e138a470471521db17b98b844d7bd21ee91e
Kodpi <@isamposbe0>
2024-07-04 11:45:28 +0300

    Написал комментарии для кода

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Service\TournamentService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,12 +15,15 @@ use Symfony\Component\Routing\Attribute\Route;
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, TournamentService $service): Response
     {
+        /** Создаем пользователя */
         $user = new User();
+        /** Создаем форму для регистрации */
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
+        /** Проверяем нажатие кнопки и валидность данных */
         if ($form->isSubmitted() && $form->isValid()) {
             // encode the plain password
             $user->setPassword(
@@ -38,6 +41,9 @@ class RegistrationController extends AbstractController
             return $security->login($user, 'form_login', 'main');
         }
 
+        /** Отправляем данные в шаблон
+         * @registrationForm Данные формы регистрации
+         */
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form,
         ]);
