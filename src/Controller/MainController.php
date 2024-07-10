11af133f11347bcd33a72d97bb931621aa3303a9
Kodpi <@isamposbe0>
2024-07-10 10:55:09 +0300

    Написана логика контролера для главной страницы не авторизованного пользователя

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Service\PostgresqlDBService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
+    /** Главная страница для не авторизованного пользователя
+     * @param PostgresqlDBService $DBService Сервис по работе с базой данных
+     * @return Response
+     */
     #[Route('/main', name: 'app_main')]
-    public function index(): Response
+    public function index(PostgresqlDBService $DBService): Response
     {
+        $listTournaments = $DBService->listTournaments();
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            'listTournaments' => $listTournaments
         ]);
     }
 }
