ae52180b0e9a73d017020e6abc3800af20689bdd
Kodpi <@isamposbe0>
2024-07-10 17:35:06 +0300

    Убрал лишние

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -13,8 +13,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[IsGranted('ROLE_USER')]
 #[Route('/game')]
+#[IsGranted('ROLE_USER')]
 class GameController extends AbstractController
 {
     /** Список всех игр
