81cbbbc0b306e5c32400c5c73457bdc265f8333d
Kodpi <@isamposbe0>
2024-07-10 17:35:13 +0300

    Убрал лишние

--- src/Service/CalculationService.php
+++ src/Service/CalculationService.php
@@ -16,35 +16,6 @@ class CalculationService
         private readonly TournamentService $tournamentService,
 
     ){}
-    /**
-     * @param array $a
-     * @param array $b
-     * @return array[]
-     */
-    public function matrixTour(array $listTeam): array
-    {
-
-        $countlistTeam = count($listTeam);
-        $matrixTour = [$countlistTeam][$countlistTeam];
-        for ($i = 0; $i < $countlistTeam; $i++) {
-            $matrixTour[0][0] = $listTeam[$i];
-        }
-        $flowers = [ [ "Название" => "фиалки",
-            "Стоимость" => 100,
-            "Количество" => 15
-        ],
-            [ "Название" => "астры",
-                "Стоимость" => 60,
-                "Количество" => 25,
-            ],
-            [ "Название" => "каллы",
-                "Стоимость" => 180,
-                "Количество" => 7
-            ]
-        ];
-
-        return $flowers;
-    }
 
     /** Генерация Игр в турнире
      * @param Tournament $tournament Данные турнира
